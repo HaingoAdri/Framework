317a41540fa29290abfa6d46bddc7edaed9c1437
HaingoAdri <120141604+HaingoAdri@users.noreply.github.com>
2023-03-14 20:42:18 +1400

    Add files via upload

---
+++ Mapping/WEB-INF/Mapping.java
@@ -0,0 +1,28 @@
+package etu2069.framework;
+public class Mapping {
+    String className;
+    String method;
+
+    public Mapping(){}
+
+    public Mapping(String className, String method){
+        this.setClassName(className);
+        this.setMethod(method);
+    }
+
+    public String getClassName() {
+        return this.className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return this.method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    
+}
